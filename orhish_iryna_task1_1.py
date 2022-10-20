04eba4f8241277aae316554736ce73de559c6682
justihere <107419759+justihere@users.noreply.github.com>
2022-10-20 10:17:17 +0200

    Update orhish_iryna_task1_1.py

--- orhish_iryna_task1_1.py
+++ orhish_iryna_task1_1.py
@@ -1,55 +1,87 @@
+
 class Client:
-    def __init__(self, name, surname, fathers_name, number_porposition, sum, date):
-      self.name = name
-      self.surname = surname
-      self.fathers_name = fathers_name
-      self.number_porposition = number_porposition
-      self.sum = sum
-      self.date = date
+    def __init__(self, new_name, new_surname, new_fathers_name, New_number_porposition, new_sum_val, new_date):
+        self.name = new_name
+        self.surname = new_surname
+        self.fathers_name = new_fathers_name
+        self.number_porposition = New_number_porposition
+        self.sum_val = new_sum_val
+        self.date = new_date
+
+    def __str__(self):
+        return f'name     : {self.__name}\
+               \nsurname    : {self.__surname}\
+               \nfathers_name   : {self.__sfathers_name}\
+               \nnumber_porposition    : {self.__number_porposition}\
+               \nsum: {self.__sum_val}\
+               \ndate     : {self.__date}'
 
     @property
-    def get_fathers_name(self):
-        return self.__fathers_name
-
-    @property
-    def get_number_porposition(self):
-        return self.__number_porposition
-
-    @property
-    def get_date(self):
-        return self.__date
-
-    @property
-    def get_name(self):
+    def name(self):
         return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError('Only type str allowed for name.')
+        self.__name = new_name
 
     @property
     def surname(self):
         return self.__surname
 
-    def setter(self, name, surname, fathers_name, number_porposition, sum, date):
-        if not isinstance(name, str):
-            raise TypeError('Name should be string data type!')
-        if not isinstance(surname, str):
-            raise TypeError('Surname should be string data type!')
-        if not isinstance(fathers_name, str):
-            raise TypeError('Name should be string data type!')
-        if not all(isinstance(x, int) for x in number_porposition.values()):
-            raise TypeError('Grades values should be int type!')
-        if not all(isinstance(x, int) for x in sum.keys()):
-            raise TypeError('Grades keys should be string type!')
-        if not all(isinstance(x, int) for x in sum.keys()):
-            raise TypeError('Grades keys should be string type!')
-        if not isinstance(date, str):
-            raise TypeError('Surname should be string data type!')
+    @surname.setter
+    def surname(self, new_surname):
+        if not isinstance(new_surname, str):
+            raise TypeError('Only type str allowed for surname.')
+        self.__surname = new_surname
 
+    @property
+    def fathers_name(self):
+        return self.__fathers_name
+
+    @fathers_name.setter
+    def fathers_name(self, new_fathers_name):
+        if not isinstance(new_fathers_name, str):
+            raise TypeError('Only type str allowed for fathers_name.')
+        self.__fathers_name = new_fathers_name
+
+    @property
+    def number_porposition(self):
+        return self.__number_porposition
+
+    @number_porposition.setter
+    def number_porposition(self, New_number_porposition):
+        if not isinstance(New_number_porposition, int):
+            raise TypeError('Only type str allowed for number_porposition.')
+        self.__number_porposition = New_number_porposition
+
+    @property
+    def sum_val(self):
+        return self.__sum_val
+
+    @sum_val.setter
+    def sum_val(self, new_sum):
+        if not isinstance(new_sum, int | float):
+            raise TypeError('Only types int and float allowed for sum.')
+        self.__sum_val = new_sum
+
+    @property
+    def date(self):
+        return self.__date
+
+    @date.setter
+    def date(self, new_date):
+        if not isinstance(new_date, str):
+            raise TypeError('Only type str allowed for date.')
+        self.__date = new_date
 
 
     def year(self):
-        sum *= 0.15
+       s = (self.sum_val * 15 * (273)/365)/100
+       print(s)
 
-        print( sum)
 
+cli1 = Client('Jack', 'Smith', 'Tom', 15, 4700.55, '1.10.2022')
 
-client = Client("Jack", "Smith", "Tom", 15, 4700, 1102022)
-client.year()
+cli1.year()
